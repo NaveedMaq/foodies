1ae42e6175f009dfe0db94977f25566d0fe9ce9f
NaveedMaq <naveedmaqbool30@gmail.com>
2023-12-15 14:40:40 +0530

    add meals comp

--- components/index.tsx
+++ components/index.tsx
@@ -1,2 +1,3 @@
 export { default as MainHeader } from "./main-header/main-header";
 export { default as ImageSlideShow } from "./images/image-slideshow";
+export { default as MealsGrid } from "./meals/meals-grid";
